90702fd00da3012dcc7004141d989446f71ac136
robbie <robbiekruszynski@gmail.com>
2023-10-28 20:20:32 +0200

    need to fix linklist still

--- src/components/Linktree.js
+++ src/components/Linktree.js
@@ -2,21 +2,32 @@ import React, { useEffect } from 'react';
 import { Button, Container, Typography, Avatar } from '@mui/material';
 import { Box } from '@mui/system';
 import caronImage from '../assets/images/caron.png';
+import backgroundImage from '../assets/images/m2.png';  
 import './Linktree.css';
 
 const Linktree = () => {
   useEffect(() => {
-    document.body.style.backgroundColor = 'grey';
+    document.body.style.backgroundImage = `url(${backgroundImage})`;
+    document.body.style.backgroundSize = 'cover';
+    document.body.style.backgroundPosition = 'center';
+    document.body.style.margin = 0;
+    document.body.style.height = '100vh';
+    document.body.style.width = '100%';
     return () => {
-      document.body.style.backgroundColor = null;
+      document.body.style.backgroundImage = null;
+      document.body.style.backgroundSize = null;
+      document.body.style.backgroundPosition = null;
+      document.body.style.margin = null;
+      document.body.style.height = null;
+      document.body.style.width = null;
     };
   }, []);
 
   const links = [
-    { title: 'Telegram', url: 'https://t.me/caronfire', background: 'linear-gradient(45deg, #009688 30%, #4db6ac 90%)' },
-    { title: 'Twitter', url: 'https://twitter.com/caronfiree', background: 'linear-gradient(45deg, #009688 30%, #4db6ac 90%)' },
-   { title: 'GitHub', url: 'https://github.com/CaronSch', background: 'linear-gradient(45deg, #009688 30%, #4db6ac 90%)' },
-    { title: 'LinkedIn', url: 'https://www.linkedin.com/in/caronschaller/', background: 'linear-gradient(45deg, #009688 30%, #4db6ac 90%)' },
+    { title: 'Telegram', url: 'https://t.me/caronfire' },
+    { title: 'Twitter', url: 'https://twitter.com/caronfiree' },
+    { title: 'GitHub', url: 'https://github.com/CaronSch' },
+    { title: 'LinkedIn', url: 'https://www.linkedin.com/in/caronschaller/' },
   ];
 
   return (
@@ -27,22 +38,22 @@ const Linktree = () => {
         alignItems="center"
         justifyContent="center"
         minHeight="100vh"
+        minWidth="100vw"
         style={{ color: 'white' }}
       >
         <Avatar 
           alt="Profile Photo" 
           src={caronImage}
-          sx={{ width: '120px', height: '120px', marginBottom: '20px' }}  // Increased padding between circle and text
+          sx={{ width: '120px', height: '120px', marginBottom: '20px' }}  
         />
-        <Typography variant="h4" component="h1" gutterBottom>
+        <Typography variant="h4" component="h1" gutterBottom color="black">
           CarOnFire
         </Typography>
         {links.map((link, index) => (
-          <Box mt={3} width={1 / 3} key={index}>  {/* Shorten the length of the buttons */}
+          <Box mt={3} width={1 / 3} key={index}>
             <Button
-              className="angled-button"  // Updated className
+              className="angled-button"
               variant="contained"
-              color="primary"
               fullWidth
               onClick={() => window.open(link.url, '_blank')}
             >
